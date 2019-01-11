affc7d18a9dad4032662d96367be1141e04aec54
github username <github email address>
2019-01-11 15:08:56 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,24 +7,18 @@ class Song
   @@songs = []
 
   def initialize
-    @@songs << self
+    super
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  extend Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
 
   def artist=(artist)
     @artist = artist
